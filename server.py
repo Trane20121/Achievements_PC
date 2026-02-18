37eb730534c57e211c9583dfe47f5aa66679e4fa
Trane20121 <hermes.demicheli@hotmail.com>
2026-02-18 18:54:26 +0100

    Fixed multi auto open browser

--- server.py
+++ server.py
@@ -383,5 +383,5 @@ def api_logout():
 
 
 if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    webbrowser.open("http://127.0.0.1:5000")  
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
